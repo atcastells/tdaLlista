5644f049abb3b979368ae48a4fd9e9a321667cc6
atcastells <aron.deltebre@gmail.com>
2016-02-26 10:26:31 +0100

    Aplicant interface

--- src/tdaLlista.java
+++ src/tdaLlista.java
@@ -13,7 +13,7 @@ public class tdaLlista {
 
     public final int CAPACITAT= 100;
     public String[] llistaArray = new String[CAPACITAT];
-    public int total = 0;
+    public int quantitat = 0;
 
     public static void main(String[] args) {
         new tdaLlista().inici();
@@ -38,7 +38,7 @@ public class tdaLlista {
             opcio = funcioMenu(menu);
             switch (opcio) {
                 case 1:
-                       int  posicio = readInt("Introdueix una posició entre "+0+" i "+total+": ");
+                       int  posicio = readInt("Introdueix una posició entre "+0+" i "+quantitat+": ");
                     String cognom = readString("Introdueix un Cognom: ");
                     if (inserir(cognom, posicio,llistaArray)) {
                         imprimir("S'ha inserit correctament");
@@ -64,7 +64,7 @@ public class tdaLlista {
                         imprimir("No hi han dades en aquesta posició.");
                     }
                     else{
-                        imprimir("A la posició nº"+posicio+" estaba el cognom: "+cognom);
+                        imprimir("A la posició nº"+posicio+" està el cognom: "+cognom);
                     }
                     break;
                 case 4:
@@ -96,7 +96,9 @@ public class tdaLlista {
                     ordena();
                     break;*/
             }
-            enterContinuar();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("\nPrem enter per continuar... ");
+            sc.nextLine();
         }
     }
 
@@ -105,15 +107,15 @@ public class tdaLlista {
         if(!potInserir(posicio)){
             return false;
         }
-        if (llistaBuida()) {
+        if (isEmpty()) {
             cognoms[posicio] = cognom;
-            total++;
+            quantitat++;
             return true;
         }
         else{
             desplaçarLlista(posicio);
            cognoms[posicio] = cognom;
-            total++;
+            quantitat++;
             return true;
         }
 
@@ -121,11 +123,11 @@ public class tdaLlista {
 
     /*Funció Imprimir Array*/
     void imprimirArray(){
-        if(llistaBuida()){
+        if(isEmpty()){
             imprimir("La llista està buida");
         }
         else {
-            for (int i = 0; i < total; i++) {
+            for (int i = 0; i < quantitat; i++) {
                 imprimir("#" + i + "\t" + llistaArray[i]);
             }
         }
@@ -134,7 +136,7 @@ public class tdaLlista {
     /*Funció Localitzar*/
     int localitzar(String cognom){
         int posicio = 0;
-        for (int i = 0; i < total; i++){
+        for (int i = 0; i < quantitat; i++){
             if(llistaArray[i].equalsIgnoreCase(cognom)){
                 return i;
             }
@@ -144,7 +146,7 @@ public class tdaLlista {
 
     /*Funció recuperar*/
     String recuperar(int posicio){
-        if(posicio < total && !llistaBuida()){
+        if(posicio < quantitat && !isEmpty()){
             return llistaArray[posicio];
         }
         else{
@@ -154,21 +156,38 @@ public class tdaLlista {
 
     /*Funció Suprimir*/
     boolean suprimir(int posicio){
-        if (llistaBuida() || !potEliminar(posicio)){
+        if (isEmpty() || !limit(posicio)){
             return false;
         }
         else{
-            if(posicio == total){
-                total--;
+            if(posicio == quantitat){
+                quantitat--;
                 return true;
             }
             else{
                 eliminarPosicio(posicio);
+                quantitat--;
                 return true;
             }
         }
     }
-    /*Funcions auxiliars*/
+
+    /*###############################################
+    Funcions aux. tdaLlista
+    ###############################################*/
+
+    boolean isFull(){
+        return quantitat == CAPACITAT;
+    }
+
+    boolean isEmpty(){
+        return quantitat == 0;
+    }
+
+    /*###############################################
+    Funcions auxiliars
+    ###############################################*/
+
     int funcioMenu(String[] menu) {                                     //Retorna la opcio del menu
         for (int i = 0; i < menu.length; i++) {
             System.out.println(menu[i]);
@@ -177,15 +196,15 @@ public class tdaLlista {
         return opcio;
     }
 
-    boolean potInserir(int posicio) {                                   //Boolea per cuan no es pot inserir
-        if (total >= 0 && posicio <= total || total == MAXIM) {
+    boolean potInserir(int posicio) {                                   //Boolea per cuan es pot inserir
+        if (quantitat >= 0 && posicio <= quantitat || quantitat == CAPACITAT) {
             return true;
         } else {
             return false;
         }
     }
-    boolean potEliminar(int posicio) {                                   //Boolea per cuan no es pot eliminar
-        if (total == 0 && posicio == 0 || total > 0 && posicio < total || total == MAXIM) {
+    boolean limit(int posicio) {                                   //Boolea per cuan no es pot eliminar
+        if (posicio >= 0 && posicio < quantitat) {
             return true;
         } else {
             return false;
@@ -211,31 +230,17 @@ public class tdaLlista {
         return newString;
     }
 
-    boolean llistaBuida() {                  //Funció per saber si la llista està buida
-        if (total == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     void imprimir(String text) {             //Funció per imprimir un missatge
         System.out.print(text+"\n");
     }
 
-     void enterContinuar() {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("\nPrem enter per continuar... ");
-         sc.nextLine();
-     }
-
     void desplaçarLlista(int posicio){                  //Funció per lliberar un espai a la array;
-        for(int i = total;i > posicio;i--){
+        for(int i = quantitat;i > posicio;i--){
             llistaArray[i] = llistaArray[i-1];
         }
     }
     void eliminarPosicio(int x){
-        for(int i = x ; i < total ;i++){
+        for(int i = x ; i < quantitat ;i++){
             llistaArray[i] = llistaArray[i+1];
         }
     }
